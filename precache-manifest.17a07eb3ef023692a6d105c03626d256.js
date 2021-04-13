f89b9f66febf89acd1c206b9e4f5bc5f27fb5a0d
NaviduRoshika <navidu.18@itfac.mrt.ac.lk>
2021-04-13 16:42:48 +0530

    Updates

---
+++ precache-manifest.17a07eb3ef023692a6d105c03626d256.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61345a0c68cf0f34db6a5a69368090bf",
+    "url": "/space-ladders/index.html"
+  },
+  {
+    "revision": "a0421d51d8f94070ef99",
+    "url": "/space-ladders/static/css/2.bada8f97.chunk.css"
+  },
+  {
+    "revision": "6e0b46c6cce6f8233d03",
+    "url": "/space-ladders/static/css/main.b78524f8.chunk.css"
+  },
+  {
+    "revision": "a0421d51d8f94070ef99",
+    "url": "/space-ladders/static/js/2.c591817a.chunk.js"
+  },
+  {
+    "revision": "e420ff54aaee9ec65dfee3e735742ec2",
+    "url": "/space-ladders/static/js/2.c591817a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0b46c6cce6f8233d03",
+    "url": "/space-ladders/static/js/main.cef11301.chunk.js"
+  },
+  {
+    "revision": "6d4a6b6d79b68933bd61",
+    "url": "/space-ladders/static/js/runtime-main.366b2ae7.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/space-ladders/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "97cf18f738bfe1c9ac097a52181ee547",
+    "url": "/space-ladders/static/media/click2.97cf18f7.mp3"
+  },
+  {
+    "revision": "e0ba8ae92a3984b18b30f0924e5bd8d2",
+    "url": "/space-ladders/static/media/dice.e0ba8ae9.gif"
+  },
+  {
+    "revision": "cdc9f363f1651338bea32eb1d540c103",
+    "url": "/space-ladders/static/media/five.cdc9f363.png"
+  },
+  {
+    "revision": "7b74235b2822bd6935cd5d9483bf0ec1",
+    "url": "/space-ladders/static/media/four.7b74235b.png"
+  },
+  {
+    "revision": "8444f3460695f9bec8695a45bbb5934a",
+    "url": "/space-ladders/static/media/gamePlay.8444f346.mp3"
+  },
+  {
+    "revision": "b45aa86cf856664af8aeb4231adbbc1d",
+    "url": "/space-ladders/static/media/icon3.b45aa86c.png"
+  },
+  {
+    "revision": "fe95417f0dd7db835c0ea5b9d5318157",
+    "url": "/space-ladders/static/media/icon4.fe95417f.png"
+  },
+  {
+    "revision": "4a354d4b5f272aa909c296aae4b03348",
+    "url": "/space-ladders/static/media/iconB.4a354d4b.png"
+  },
+  {
+    "revision": "bd5ff1c2472f754db326702ce043d06d",
+    "url": "/space-ladders/static/media/iconW.bd5ff1c2.png"
+  },
+  {
+    "revision": "83a7d15c8040cdfe82dadbab295e09e6",
+    "url": "/space-ladders/static/media/intro.83a7d15c.mp3"
+  },
+  {
+    "revision": "ccb88fffd68647cecc312e1bdcf1d88f",
+    "url": "/space-ladders/static/media/one.ccb88fff.png"
+  },
+  {
+    "revision": "3c3a4241042095518916ad677848f577",
+    "url": "/space-ladders/static/media/six.3c3a4241.png"
+  },
+  {
+    "revision": "a49ab82b4c6423c0a274897b9f778f29",
+    "url": "/space-ladders/static/media/three.a49ab82b.png"
+  },
+  {
+    "revision": "b58ddbfc380cf5a8848b2cdba3a1b566",
+    "url": "/space-ladders/static/media/two.b58ddbfc.png"
+  }
+]);
